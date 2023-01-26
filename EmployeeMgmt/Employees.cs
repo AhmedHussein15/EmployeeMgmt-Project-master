85a848c1f2db21b28f0efa06637d64c5a7f23470
LENOVO <LENOVO@LAPTOP-NFQIQ875>
2023-01-26 14:11:04 +0200

    finished with the first desgin of the login page we now starting

--- EmployeeMgmt/Employees.cs
+++ EmployeeMgmt/Employees.cs
@@ -1,25 +1,25 @@
-﻿using System; 
-using System.Collections.Generic; 
-using System.ComponentModel; 
-using System.Data; 
-using System.Drawing; 
-using System.Linq; 
-using System.Text; 
-using System.Threading.Tasks; 
-using System.Windows.Forms; 
+﻿using System;  
+using System.Collections.Generic;   
+using System.ComponentModel;  
+using System.Data;  
+using System.Drawing;   
+using System.Linq;  
+using System.Text;  
+using System.Threading.Tasks;  
+using System.Windows.Forms;  
 
-namespace EmployeeMgmt 
-{ 
-    Functions Con; 
-    public partial class Employees : Form 
+namespace EmployeeMgmt  
+{  
+    Functions Con;  
+    public partial class Employees : Form  
     { 
-        public Employees() 
+        public Employees()  
         { 
-            InitializeComponent(); 
-            Con = new Functions();
-            ShowEmp();
-            GetDepartment();
-        }
+            InitializeComponent();  
+            Con = new Functions(); 
+            ShowEmp(); 
+            GetDepartment(); 
+        } 
         private void ShowEmp()
         {
             string Query = "Select * from EmployeeTb1";
