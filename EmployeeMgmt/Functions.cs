d567742c19a8aaad1e784b4a60efa5742aeae892
LENOVO <LENOVO@LAPTOP-NFQIQ875>
2023-01-26 15:07:56 +0200

    create connection from data base

--- EmployeeMgmt/Functions.cs
+++ EmployeeMgmt/Functions.cs
@@ -17,31 +17,31 @@ namespace EmployeeMgmt
         private string ConStr;
 
 
-        public Functions()  {
+        public Functions()  { 
             ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ahmed\Documents\EmpDb.mdf;Integrated Security=True;Connect Timeout=30";
-            Con = new SqlConnection(ConStr);
-            Cmd = new SqlCommand();
-            Cmd.Connection = Con;
+            Con = new SqlConnection(ConStr); 
+            Cmd = new SqlCommand(); 
+            Cmd.Connection = Con; 
 
-        }
+        } 
 
-        public DataTable GetDataTable(string Query) {
-            dt = new DataTable();
-            sda = new SqlDataAdapter(Query, ConStr); 
-            sda.Fill(dt);
-            return dt;
-        }
+        public DataTable GetDataTable(string Query) { 
+            dt = new DataTable(); 
+            sda = new SqlDataAdapter(Query, ConStr);  
+            sda.Fill(dt); 
+            return dt; 
+        } 
 
-        public int SetData(string Query) 
-        {
-            int cnt = 0;
-            if (Con.State == ConnectionState.Closed) {
-                Con.Open();
-            }
-            Cmd.CommandText = Query;
-            cnt = Cmd.ExecuteNonQuery();
-            return cnt;
-        }
+        public int SetData(string Query)  
+        { 
+            int cnt = 0; 
+            if (Con.State == ConnectionState.Closed) { 
+                Con.Open(); 
+            } 
+            Cmd.CommandText = Query; 
+            cnt = Cmd.ExecuteNonQuery(); 
+            return cnt; 
+        } 
 
-    }
+    } 
 }
