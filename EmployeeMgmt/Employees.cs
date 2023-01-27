f5f4b0c1434ae2228975a6d366a7925f5946be7f
LENOVO <LENOVO@LAPTOP-NFQIQ875>
2023-01-27 21:16:21 +0200

    DepCb.ValueMember = Con.GetData(Query).Columns["DepId"].ToString()

--- EmployeeMgmt/Employees.cs
+++ EmployeeMgmt/Employees.cs
@@ -28,7 +28,7 @@ namespace EmployeeMgmt
         private void GetDepartment() {
             string Query = "select * from DepartmentTb1";
             DepCb.DisplayMember = Con.GetData(Query).Columns("DepName").ToString();
-            DepCb.ValueMember = Con.GetData(Query).Columns["DepId"].ToString();
+            DepCb.ValueMember = Con.GetData(Query).Columns["DepId"].ToString(); 
             DepCb.DataSource = Con.GetData(Query);
 
         }
